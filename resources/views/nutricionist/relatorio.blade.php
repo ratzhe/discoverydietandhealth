f062ea640161472d654b18992b5d50843004722d
nicholas <nicholas.zaze@gmail.com>
2024-09-24 21:06:59 -0300

    message

--- resources/views/nutricionist/relatorio.blade.php
+++ resources/views/nutricionist/relatorio.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-5">
     <div class="container">
     <h2>Evolução dos Pacientes</h2>
-        <iframe width="1100" height="2125" src="https://lookerstudio.google.com/embed/reporting/b68539f4-c0fb-45bf-b227-e7caf5fd09d7/page/kIV1C" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+    <iframe width="1000" height="2125" src="https://lookerstudio.google.com/embed/reporting/1dea7624-c7e1-4d92-a083-ea90e5ac4663/page/kIV1C" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
     </div>
 </div>
 @endsection
