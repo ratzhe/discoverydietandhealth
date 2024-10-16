8201b41e9fe1918c39c550ec68e3f85fa347e634
nicholas <nicholas.zaze@gmail.com>
2024-10-16 00:21:55 -0300

    mudanças no dash e inclusão da tag de GA 2

--- resources/views/nutricionist/relatorio.blade.php
+++ resources/views/nutricionist/relatorio.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-5">
     <div class="container">
     <h2>Evolução dos Pacientes</h2>
-    <iframe width="1000" height="2125" src="https://lookerstudio.google.com/embed/reporting/1dea7624-c7e1-4d92-a083-ea90e5ac4663/page/kIV1C" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+    <iframe    src="http://localhost:3000/public/dashboard/5644658a-317e-4aa1-bd50-1861fdbf33ba"    frameborder="0"    width="1200"    height="2000"    allowtransparency></iframe>
     </div>
 </div>
 @endsection
